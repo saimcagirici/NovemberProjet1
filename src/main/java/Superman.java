82a98a9fa2181438290db875a4b5b5dec4df0994
saimcagirici <cagirici@live.com>
2020-11-01 11:23:26 -0500

    print on batman page gotman

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -3,5 +3,8 @@ public class Superman {
         System.out.println("Superman");
         ///
         ///
+        ///
+        ///
+        ///
     }
 }
--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -4,6 +4,9 @@ public class Batman {
         System.out.println("Batman");
         ///
         ///
+        ////
+        ///
+
 
     }
 }
